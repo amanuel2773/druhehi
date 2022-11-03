6b68f8ae26f873dd0efa7b4f9772c05308cae781
amanuel2773 <117325134+amanuel2773@users.noreply.github.com>
2022-11-03 09:03:56 +0800

    Add files via upload

---
+++ WEBAPP/proposalv2.js
@@ -0,0 +1,256 @@
+
+
+const no = document.getElementById('btn2');
+const yes = document.getElementById('btn1');
+
+//1st group of btts
+
+const btt1 = document.getElementById('btt1');
+const btt2 = document.getElementById('btt2');
+const btt3 = document.getElementById('btt3');
+const btt4 = document.getElementById('btt4');
+const btt5 = document.getElementById('btt5');
+const btt6 = document.getElementById('btt6');
+const btt7 = document.getElementById('btt7');
+const btt8 = document.getElementById('btt8');
+const btt9 = document.getElementById('btt9');
+const btt10 = document.getElementById('btt10');
+const btt11 = document.getElementById('btt11');
+const btt12 = document.getElementById('btt12');
+const btt13 = document.getElementById('btt13');
+
+//2nd group of btts
+
+const bbtt1 = document.getElementById('bbtt1');
+const bbtt2 = document.getElementById('bbtt2');
+const bbtt3 = document.getElementById('bbtt3');
+const bbtt4 = document.getElementById('bbtt4');
+const bbtt5 = document.getElementById('bbtt5');
+const bbtt6 = document.getElementById('bbtt6');
+const bbtt7 = document.getElementById('bbtt7');
+const bbtt8 = document.getElementById('bbtt8');
+const bbtt9 = document.getElementById('bbtt9');
+const bbtt10 = document.getElementById('bbtt10');
+const bbtt11 = document.getElementById('bbtt11');
+const bbtt12 = document.getElementById('bbtt12');
+
+
+//IMAGES
+
+const hands = document.getElementById('hands');
+const sadcat = document.getElementById('sadcat');
+const crying = document.getElementById('crying');
+const sadboi = document.getElementById('sadboi');
+const sadman = document.getElementById('sadman');
+const hahahuhu = document.getElementById('hahahuhu');
+const hang = document.getElementById('hang');
+const sadsad = document.getElementById('sadsad');
+const frog = document.getElementById('frog');
+
+btt1.style.opacity= "0";
+btt2.style.opacity= "0";
+btt3.style.opacity= "0";
+btt4.style.opacity= "0";
+btt5.style.opacity= "0";
+btt6.style.opacity= "0";
+btt7.style.opacity= "0";
+btt8.style.opacity= "0";
+btt9.style.opacity= "0";
+btt10.style.opacity= "0";
+btt11.style.opacity= "0";
+btt12.style.opacity= "0";
+bbtt1.style.opacity= "0";
+bbtt2.style.opacity= "0";
+bbtt3.style.opacity= "0";
+bbtt4.style.opacity= "0";
+bbtt5.style.opacity= "0";
+bbtt6.style.opacity= "0";
+bbtt7.style.opacity= "0";
+bbtt8.style.opacity= "0";
+bbtt9.style.opacity= "0";
+bbtt10.style.opacity= "0";
+bbtt11.style.opacity= "0";
+bbtt12.style.opacity= "0";
+crying.style.opacity="0";
+hang.style.opacity="0";
+frog.style.opacity="0";
+hahahuhu.style.opacity="0";
+sadsad.style.opacity="0";
+sadcat.style.opacity="0";
+sadman.style.opacity="0";
+sadboi.style.opacity="0";
+btt13.style.opacity="0";
+
+no.addEventListener("click", function (){
+    no.style.opacity= "0";
+
+    btt11.style.opacity= "1";
+
+
+});
+
+btt1.addEventListener("click", function (){
+    btt1.style.opacity= "0";
+    btt4.style.opacity= "1";
+    crying.style.opacity="1";
+});
+
+btt2.addEventListener("click", function (){
+    btt2.style.opacity= "0";
+    btt7.style.opacity= "1";
+    hang.style.opacity="1";
+    yes.style.padding="75px"
+});
+
+btt3.addEventListener("click", function (){
+    btt3.style.opacity= "0";
+    bbtt7.style.opacity= "1";
+    yes.style.padding="160px"
+
+});
+
+btt4.addEventListener("click", function (){
+    btt4.style.opacity= "0";
+    bbtt9.style.opacity= "1";
+    frog.style.opacity="1";
+});
+
+btt5.addEventListener("click", function (){
+    btt5.style.opacity= "0";
+    bbtt12.style.opacity= "1";
+    yes.style.padding="150px"
+
+});
+
+btt6.addEventListener("click", function (){
+    btt6.style.opacity= "0";
+    bbtt11.style.opacity= "1";
+
+});
+
+btt7.addEventListener("click", function (){
+    btt7.style.opacity= "0";
+    btt12.style.opacity="1";
+    yes.style.padding="100px"
+
+});
+btt8.addEventListener("click", function (){
+    btt8.style.opacity= "0";
+    bbtt3.style.opacity= "1";
+    yes.style.padding="140px"
+
+});
+
+btt9.addEventListener("click", function (){
+    btt9.style.opacity= "0";
+    btt13.style.opacity="1";
+
+
+});
+
+btt10.addEventListener("click", function (){
+    btt10.style.opacity= "0";
+    bbtt5.style.opacity= "1";
+    
+
+});
+
+btt11.addEventListener("click", function (){
+    btt11.style.opacity= "0";
+    bbtt2.style.opacity= "1";
+    hahahuhu.style.opacity="1";
+
+});
+
+btt12.addEventListener("click", function (){
+    btt12.style.opacity= "0";
+    bbtt4.style.opacity= "1";
+    yes.style.padding="125px"
+
+});
+
+bbtt1.addEventListener("click", function (){
+    bbtt1.style.opacity= "0";
+    bbtt10.style.opacity= "1";
+    yes.style.padding="135px"
+
+});
+
+bbtt2.addEventListener("click", function (){
+    bbtt2.style.opacity= "0";
+    bbtt8.style.opacity= "1";
+    sadsad.style.opacity="1";
+});
+
+bbtt3.addEventListener("click", function (){
+    bbtt3.style.opacity= "0";
+    btt5.style.opacity= "1";
+    yes.style.padding="145px"
+
+});
+
+bbtt4.addEventListener("click", function (){
+    bbtt4.style.opacity= "0";
+    bbtt1.style.opacity= "1";
+    yes.style.padding="130px"
+
+});
+
+bbtt5.addEventListener("click", function (){
+    bbtt5.style.opacity= "0";
+    btt6.style.opacity= "1";
+
+});
+
+bbtt6.addEventListener("click", function (){
+    bbtt6.style.opacity= "0";
+    btt2.style.opacity= "1";
+    sadcat.style.opacity="1";
+    yes.style.padding="50px";
+
+});
+
+bbtt7.addEventListener("click", function (){
+    bbtt7.style.opacity= "0";
+    btt10.style.opacity= "1";
+
+});
+
+bbtt8.addEventListener("click", function (){
+    bbtt8.style.opacity= "0";
+    btt1.style.opacity= "1";
+    sadman.style.opacity="1";
+
+});
+
+bbtt9.addEventListener("click", function (){
+    bbtt9.style.opacity= "0";
+    bbtt6.style.opacity= "1";
+    yes.style.padding="25px";
+    sadboi.style.opacity="1";
+
+});
+
+bbtt10.addEventListener("click", function (){
+    bbtt10.style.opacity= "0";
+    btt8.style.opacity= "1";
+});
+
+bbtt11.addEventListener("click", function (){
+    bbtt11.style.opacity= "0";
+    btt9.style.opacity="1";
+
+});
+
+bbtt12.addEventListener("click", function (){
+    bbtt12.style.opacity= "0";
+    btt3.style.opacity= "1";
+
+});
+
+
+
+
+
+
+
